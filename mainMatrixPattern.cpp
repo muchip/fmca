4d727b984ae57285951f73cd04c9f0d22373e692
mm <muchip@protonmail.com>
2021-06-28 20:38:42 +0200

    fixed mainfile

--- mainMatrixPattern.cpp
+++ mainMatrixPattern.cpp
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
   const double svd_threshold = 1e-6;
   const double aposteriori_threshold = 1e-6;
   const double ridge_param = 1e-3;
-  const std::string logger = "loggerBenchmark3DSVD.txt";
+  const std::string logger = "loggerBenchmark3DQR.txt";
   tictoc T;
   {
     std::ifstream file;
@@ -95,115 +95,139 @@ int main(int argc, char *argv[]) {
   // const unsigned int npts = 1 << i;
   //  Eigen::MatrixXd P = Eigen::MatrixXd::Random(DIM, npts);
   std::cout << "loading data: ";
-  Eigen::MatrixXd B = readMatrix("./Points/bunnySurface_tiny.txt");
+  Eigen::MatrixXd B = readMatrix("./Points/bunnyVolume.txt");
   std::cout << "data size: ";
   std::cout << B.rows() << " " << B.cols() << std::endl;
-  const unsigned int npts = 20000;
   Eigen::MatrixXd P = B.transpose();
-  P = P.leftCols(20000);
-  std::cout << std::string(60, '-') << std::endl;
-  std::cout << "dim:       " << DIM << std::endl;
-  std::cout << "leaf size: " << LEAFSIZE << std::endl;
-  std::cout << "mpole deg: " << MPOLE_DEG << std::endl;
-  std::cout << "dtilde:    " << DTILDE << std::endl;
-  std::cout << "npts:      " << npts << std::endl;
-  std::cout << "rparam:    " << ridge_param << std::endl;
-  std::cout << std::string(60, '-') << std::endl;
-  //////////////////////////////////////////////////////////////////////////////
-  // set up cluster tree
-  T.tic();
-  ClusterT CT(P);
-  T.toc("set up cluster tree: ");
-  //////////////////////////////////////////////////////////////////////////////
-  // set up H2 cluster tree
-  T.tic();
-  H2ClusterT H2CT(P, CT);
-  T.toc("set up H2-cluster tree: ");
-  {
-    std::vector<std::vector<FMCA::IndexType>> tree;
-    CT.exportTreeStructure(tree);
-    std::cout << "cluster structure: " << std::endl;
-    std::cout << "l)\t#pts\ttotal#pts" << std::endl;
-    for (auto i = 0; i < tree.size(); ++i) {
-      int numInd = 0;
-      for (auto j = 0; j < tree[i].size(); ++j) numInd += tree[i][j];
-      std::cout << i << ")\t" << tree[i].size() << "\t" << numInd << "\n";
-    }
+  for (auto npts = 10;;) {
+    P = P.leftCols(npts);
     std::cout << std::string(60, '-') << std::endl;
-  }
-  //////////////////////////////////////////////////////////////////////////////
-  // set up samplet tree
-  T.tic();
-  FMCA::SampletTree<ClusterT> ST(P, CT, DTILDE, svd_threshold);
-  T.toc("set up samplet tree: ");
-  T.tic();
-#ifdef USE_QR_CONSTRUCTION_
-  std::cout << "using QR construction for samplet basis\n";
-#else
-  std::cout << "using SVD construction for samplet basis\n";
-  std::cout << "SVD orthogonality threshold: " << svd_threshold << std::endl;
-#endif
-  ST.computeMultiscaleClusterBases(H2CT);
-  T.toc("set up time multiscale cluster bases");
-  std::cout << std::string(60, '-') << std::endl;
-  //////////////////////////////////////////////////////////////////////////////
-  // set up bivariate compressor
-  T.tic();
-  FMCA::BivariateCompressorH2<FMCA::SampletTree<ClusterT>> BC;
-  BC.set_eta(eta);
-  BC.set_threshold(aposteriori_threshold);
-  std::cout << "bivariate compressor: \n";
-  BC.init(P, ST, exponentialKernel());
-  std::cout << std::string(60, '-') << std::endl;
-  double ctime = T.toc("set up Samplet compressed matrix: ");
-  std::cout << std::string(60, '-') << std::endl;
-  unsigned int nz = 0;
-  unsigned int nza = 0;
-  Eigen::SparseMatrix<double> W(P.cols(), P.cols());
-  {
-    auto trips = BC.get_Pattern_triplets();
-    nz = double(trips.size()) / double(P.cols());
-    nza = double(BC.get_storage_size()) / double(P.cols());
-    std::cout << "nz per row:     " << nza << " / " << nz << std::endl;
-    std::cout << "storage sparse: "
-              << double(sizeof(double) * trips.size() * 3) / double(1e9)
-              << "GB\n";
-    std::cout << "storage full:   "
-              << double(sizeof(double) * P.cols() * P.cols()) / double(1e9)
-              << "GB" << std::endl;
-    W.setFromTriplets(trips.begin(), trips.end());
-  }
-  std::cout << std::string(60, '-') << std::endl;
-  double mom_err = 0;
-  {
-    std::cout << "testing vanishing moments:\n";
-    // compute the multi indices for the monomials used for vanishing moments
-    FMCA::MultiIndexSet<DIM> idcs(DTILDE - 1);
-    // evaluate all these monomials at the points
-    Eigen::MatrixXd Pol = FMCA::momentComputer(P, CT, idcs);
-    double err = 0;
-    for (auto i = 0; i < Pol.rows(); ++i) {
-      Pol.row(i) = ST.sampletTransform(Pol.row(i));
-      err += Pol.row(i).tail(Pol.cols() - ST.get_nscalfs()).norm();
-    }
-    mom_err = err / Pol.rows();
-    std::cout << "orthogonality error: " << mom_err << std::endl;
+    std::cout << "dim:       " << DIM << std::endl;
+    std::cout << "leaf size: " << LEAFSIZE << std::endl;
+    std::cout << "mpole deg: " << MPOLE_DEG << std::endl;
+    std::cout << "dtilde:    " << DTILDE << std::endl;
+    std::cout << "npts:      " << npts << std::endl;
+    std::cout << "rparam:    " << ridge_param << std::endl;
     std::cout << std::string(60, '-') << std::endl;
-  }
-  {
+    //////////////////////////////////////////////////////////////////////////////
+    // set up cluster tree
     T.tic();
-    Eigen::SparseMatrix<double> I(P.cols(), P.cols());
-    I.setIdentity();
-    W += ridge_param * I;
-    EigenCholesky solver;
-    solver.compute(W);
-    T.toc("Cholesky: ");
-    std::cout << "sinfo: " << (solver.info() == Eigen::Success) << std::endl;
-    std::cout << "nz Mat: " << W.nonZeros() / P.cols();
-    Eigen::SparseMatrix<double> L = solver.matrixL();
-    std::cout << " nz L: " << L.nonZeros() / P.cols() << std::endl;
+    ClusterT CT(P);
+    T.toc("set up cluster tree: ");
+    //////////////////////////////////////////////////////////////////////////////
+    // set up H2 cluster tree
+    T.tic();
+    H2ClusterT H2CT(P, CT);
+    T.toc("set up H2-cluster tree: ");
     {
+      std::vector<std::vector<FMCA::IndexType>> tree;
+      CT.exportTreeStructure(tree);
+      std::cout << "cluster structure: " << std::endl;
+      std::cout << "l)\t#pts\ttotal#pts" << std::endl;
+      for (auto i = 0; i < tree.size(); ++i) {
+        int numInd = 0;
+        for (auto j = 0; j < tree[i].size(); ++j)
+          numInd += tree[i][j];
+        std::cout << i << ")\t" << tree[i].size() << "\t" << numInd << "\n";
+      }
+      std::cout << std::string(60, '-') << std::endl;
+    }
+    //////////////////////////////////////////////////////////////////////////////
+    // set up samplet tree
+    T.tic();
+    FMCA::SampletTree<ClusterT> ST(P, CT, DTILDE, svd_threshold);
+    T.toc("set up samplet tree: ");
+    T.tic();
+#ifdef USE_QR_CONSTRUCTION_
+    std::cout << "using QR construction for samplet basis\n";
+#else
+    std::cout << "using SVD construction for samplet basis\n";
+    std::cout << "SVD orthogonality threshold: " << svd_threshold << std::endl;
+#endif
+    ST.computeMultiscaleClusterBases(H2CT);
+    T.toc("set up time multiscale cluster bases");
+    std::cout << std::string(60, '-') << std::endl;
+    //////////////////////////////////////////////////////////////////////////////
+    // set up bivariate compressor
+    T.tic();
+    FMCA::BivariateCompressorH2<FMCA::SampletTree<ClusterT>> BC;
+    BC.set_eta(eta);
+    BC.set_threshold(aposteriori_threshold);
+    std::cout << "bivariate compressor: \n";
+    BC.init(P, ST, exponentialKernel());
+    std::cout << std::string(60, '-') << std::endl;
+    double ctime = T.toc("set up Samplet compressed matrix: ");
+    std::cout << std::string(60, '-') << std::endl;
+    unsigned int nz = 0;
+    unsigned int nza = 0;
+    Eigen::SparseMatrix<double> W(P.cols(), P.cols());
+    {
+      auto trips = BC.get_Pattern_triplets();
+      nz = double(trips.size()) / double(P.cols());
+      nza = double(BC.get_storage_size()) / double(P.cols());
+      std::cout << "nz per row:     " << nza << " / " << nz << std::endl;
+      std::cout << "storage sparse: "
+                << double(sizeof(double) * trips.size() * 3) / double(1e9)
+                << "GB\n";
+      std::cout << "storage full:   "
+                << double(sizeof(double) * P.cols() * P.cols()) / double(1e9)
+                << "GB" << std::endl;
+      W.setFromTriplets(trips.begin(), trips.end());
+    }
+    std::cout << std::string(60, '-') << std::endl;
+    double mom_err = 0;
+    {
+      std::cout << "testing vanishing moments:\n";
+      // compute the multi indices for the monomials used for vanishing moments
+      FMCA::MultiIndexSet<DIM> idcs(DTILDE - 1);
+      // evaluate all these monomials at the points
+      Eigen::MatrixXd Pol = FMCA::momentComputer(P, CT, idcs);
       double err = 0;
+      for (auto i = 0; i < Pol.rows(); ++i) {
+        Pol.row(i) = ST.sampletTransform(Pol.row(i));
+        err += Pol.row(i).tail(Pol.cols() - ST.get_nscalfs()).norm();
+      }
+      mom_err = err / Pol.rows();
+      std::cout << "orthogonality error: " << mom_err << std::endl;
+      std::cout << std::string(60, '-') << std::endl;
+    }
+    {
+      T.tic();
+      Eigen::SparseMatrix<double> I(P.cols(), P.cols());
+      I.setIdentity();
+      W += ridge_param * I;
+      EigenCholesky solver;
+      solver.compute(W);
+      T.toc("Cholesky: ");
+      std::cout << "sinfo: " << (solver.info() == Eigen::Success) << std::endl;
+      std::cout << "nz Mat: " << W.nonZeros() / P.cols();
+      Eigen::SparseMatrix<double> L = solver.matrixL();
+      std::cout << " nz L: " << L.nonZeros() / P.cols() << std::endl;
+      {
+        double err = 0;
+        Eigen::VectorXd y1(P.cols());
+        Eigen::VectorXd y2(P.cols());
+        Eigen::VectorXd x(P.cols());
+        FMCA::ProgressBar PB;
+        PB.reset(10);
+        for (auto i = 0; i < 10; ++i) {
+          x.setRandom();
+          y1 = solver.permutationPinv() * L *
+               (L.transpose() * solver.permutationP() * x);
+          y2 = W * x;
+          err += (y1 - y2).norm() / y2.norm();
+          PB.next();
+        }
+        err /= 10;
+        std::cout << "\nerror: " << err << std::endl;
+        std::cout << "Wnrm2: " << get2norm(W) << " Wfnrm: " << W.norm()
+                  << std::endl;
+      }
+      W -= ridge_param * I;
+    }
+    T.tic();
+    double err = 0;
+    if (npts < 1e5) {
       Eigen::VectorXd y1(P.cols());
       Eigen::VectorXd y2(P.cols());
       Eigen::VectorXd x(P.cols());
@@ -211,47 +235,28 @@ int main(int argc, char *argv[]) {
       PB.reset(10);
       for (auto i = 0; i < 10; ++i) {
         x.setRandom();
-        y1 = solver.permutationPinv() * L *
-             (L.transpose() * solver.permutationP() * x);
-        y2 = W * x;
-        err += (y1 - y2).norm() / y2.norm();
+        y1 = matrixMultiplier(P, CT.get_indices(), exponentialKernel(), x);
+        y2 = ST.inverseSampletTransform(W * ST.sampletTransform(x));
+        err += (y1 - y2).norm() / y1.norm();
         PB.next();
       }
       err /= 10;
-      std::cout << "\nerror: " << err << std::endl;
-      std::cout << "Wnrm2: " << get2norm(W) << " Wfnrm: " << W.norm()
-                << std::endl;
+      std::cout << std::endl;
+      T.toc("time error computation: ");
+      std::cout << "error: " << err << std::endl;
     }
-    W -= ridge_param * I;
+    std::fstream newfile;
+    newfile.open(logger, std::fstream::app);
+    newfile << std::setw(10) << npts << std ::setw(5) << DIM << std::setw(8)
+            << MPOLE_DEG << std::setw(8) << DTILDE << std::setw(6) << eta
+            << std::setw(8) << aposteriori_threshold << std::setw(8)
+            << svd_threshold << std::setw(9) << nza << std::setw(9) << nz
+            << std::setw(14) << mom_err << std::setw(14) << err << std::setw(12)
+            << ctime << std::endl;
+    newfile.close();
+    if (npts == B.rows())
+      break;
+    npts = 2 * npts <= B.rows() ? 2 npts : B.rows();
   }
-  T.tic();
-  double err = 0;
-  {
-    Eigen::VectorXd y1(P.cols());
-    Eigen::VectorXd y2(P.cols());
-    Eigen::VectorXd x(P.cols());
-    FMCA::ProgressBar PB;
-    PB.reset(10);
-    for (auto i = 0; i < 10; ++i) {
-      x.setRandom();
-      y1 = matrixMultiplier(P, CT.get_indices(), exponentialKernel(), x);
-      y2 = ST.inverseSampletTransform(W * ST.sampletTransform(x));
-      err += (y1 - y2).norm() / y1.norm();
-      PB.next();
-    }
-    err /= 10;
-    std::cout << std::endl;
-    T.toc("time error computation: ");
-    std::cout << "error: " << err << std::endl;
-  }
-  std::fstream newfile;
-  newfile.open(logger, std::fstream::app);
-  newfile << std::setw(10) << npts << std ::setw(5) << DIM << std::setw(8)
-          << MPOLE_DEG << std::setw(8) << DTILDE << std::setw(6) << eta
-          << std::setw(8) << aposteriori_threshold << std::setw(8)
-          << svd_threshold << std::setw(9) << nza << std::setw(9) << nz
-          << std::setw(14) << mom_err << std::setw(14) << err << std::setw(12)
-          << ctime << std::endl;
-  newfile.close();
   return 0;
 }
