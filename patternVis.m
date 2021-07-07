4c164713587840b28d675cff2dc22ae9dc50daec
muchip <muchip@protonmail.com>
2021-07-07 09:53:38 +0200

    added pattern plotter

---
+++ patternVis.m
@@ -0,0 +1,93 @@
+clear all
+close all
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+maxNumCompThreads(1)
+bins = 512;
+[P,ctime] = MEXmainTestBenchmarkND3(17);
+S = tril(sparse(P(:,1),P(:,2),P(:,3)));
+size(S)
+S = S + 10 * speye(size(S));
+
+T = S + tril(S,-1)';
+condest(T)
+
+p = dissect(S);
+L = chol(S(p,p),'lower'); 
+
+S = S + tril(S,-1)';
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SND = S(p,p);
+n_size = size(S,1);
+csize = floor(n_size/bins)
+compS = [];
+for i=0:bins-1
+   for j=0:bins-1
+        [I,J] = find(SND(i*csize+1:(i+1)*csize,j*csize+1:(j+1)*csize) ~= 0);
+        compS(i+1,j+1) = length(I) / csize() / csize;
+   end
+end
+figure(1)
+clf;
+[X,Y] = meshgrid(1:bins);
+index = find(compS > 0);
+scatter(X(index),bins+1-Y(index),1.5,(compS(index)), 'filled')
+c = gray;
+c = flipud(c(1:200,:));
+colormap(c);
+axis equal;
+axis off;
+box on;
+title(sprintf('bins: %d, n: %d, binsize: %d, nnz: %d',...
+              bins, n_size, csize, nnz(S)));
+saveas(gcf,'Spattern.eps','epsc');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+compL = [];
+for i=0:bins-1
+   for j=0:bins-1
+        [I,J] = find(L(i*csize+1:(i+1)*csize,j*csize+1:(j+1)*csize) ~= 0);
+        compL(i+1,j+1) = length(I) / csize() / csize;
+   end
+end
+figure(2)
+clf;
+[X,Y] = meshgrid(1:bins);
+index = find(compL > 0);
+scatter(X(index),bins+1-Y(index),1.5,(compL(index)), 'filled')
+c = gray;
+c = flipud(c(1:200,:));
+colormap(c);
+axis equal;
+axis off;
+box on;
+title(sprintf('bins: %d, n: %d, binsize: %d, nnz: %d',...
+              bins, n_size, csize, nnz(L)));
+%saveas(gcf,'Lpattern.eps','epsc');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+compS = [];
+for i=0:bins-1
+   for j=0:bins-1
+        [I,J] = find(S(i*csize+1:(i+1)*csize,j*csize+1:(j+1)*csize) ~= 0);
+        compS(i+1,j+1) = length(I) / csize() / csize;
+   end
+end
+figure(3)
+clf;
+[X,Y] = meshgrid(1:bins);
+index = find(compS > 0);
+scatter(X(index),bins+1-Y(index),1.5,(compS(index)), 'filled')
+c = gray;
+c = flipud(c(1:200,:));
+colormap(c);
+axis equal;
+axis off;
+box on;
+title(sprintf('bins: %d, n: %d, binsize: %d, nnz: %d',...
+              bins, n_size, csize, nnz(S)));
+%saveas(gcf,'SWpattern.eps','epsc');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+   
