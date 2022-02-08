4e888597d91be0b1e77d24437ffad2d7f6782329
mmulterer <mmulterer@multemMBP.local>
2022-02-09 00:36:51 +0100

    update

--- examples/SwissCheese.cpp
+++ examples/SwissCheese.cpp
@@ -5,24 +5,30 @@
 #include "generateSwissCheese.h"
 
 int main() {
-  Eigen::MatrixXd P = generateSwissCheese(2, 100000, 6000);
-  Eigen::MatrixXd Q(3, 100000);
-  Q.topRows(2) = P;
   tictoc T;
+  unsigned int npts = 1e6;
+  T.tic();
+  Eigen::MatrixXd P = generateSwissCheese(3, npts);
+  T.toc("pts... ");
+  Eigen::MatrixXd Q(3, npts);
+  //Q.topRows(2) = P;
+  Q = P;
   FMCA::ClusterT CT(Q, 10);
   T.tic();
   std::vector<const FMCA::TreeBase<FMCA::ClusterT> *> leafs;
   for (auto level = 0; level < 16; ++level) {
     std::vector<Eigen::MatrixXd> bbvec;
     for (auto &node : CT) {
-      if (node.level() == level) bbvec.push_back(node.derived().bb());
+      if (node.level() == level)
+        bbvec.push_back(node.derived().bb());
     }
     FMCA::IO::plotBoxes("boxes" + std::to_string(level) + ".vtk", bbvec);
   }
   std::vector<Eigen::MatrixXd> bbvec;
 
   for (auto &node : CT) {
-    if (!node.nSons()) bbvec.push_back(node.derived().bb());
+    if (!node.nSons())
+      bbvec.push_back(node.derived().bb());
   }
   FMCA::IO::plotBoxes("boxesLeafs.vtk", bbvec);
   FMCA::IO::plotPoints("points.vtk", Q);
