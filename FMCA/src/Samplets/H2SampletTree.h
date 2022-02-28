eaae787744355f893a799a8691d0142c68619a8c
mmulterer <mmulterer@multemMini.local>
2022-02-28 12:30:10 +0100

    something is still wrong

--- FMCA/src/Samplets/H2SampletTree.h
+++ FMCA/src/Samplets/H2SampletTree.h
@@ -87,12 +87,12 @@ class H2SampletTree : public H2SampletTreeBase<H2SampletTree<ClusterTreeType>> {
         *this, mincsize, std::forward<Ts>(ts)...);
     // init hierarchical cluster basis
     internal::compute_cluster_bases_impl::compute(*this, mom);
+    //internal::compute_cluster_bases_impl::check_transfer_matrices(*this, mom);
     // init samplet basis
     computeSamplets(smom);
     internal::sampletMapper<H2SampletTree>(*this);
 
     Base::computeMultiscaleClusterBasis();
-    Base::updateMultiscaleClusterBasis(mom);
 
     return;
   }
---
+++ examples/example_H2SampletCompressor.cpp
@@ -0,0 +1,117 @@
+// This file is part of FMCA, the Fast Multiresolution Covariance Analysis
+// package.
+//
+// Copyright (c) 2021, Michael Multerer
+//
+// All rights reserved.
+//
+// This source code is subject to the BSD 3-clause license and without
+// any warranty, see <https://github.com/muchip/FMCA> for further
+// information.
+//
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+////////////////////////////////////////////////////////////////////////////////
+#include <FMCA/MatrixEvaluators>
+#include <FMCA/Samplets>
+////////////////////////////////////////////////////////////////////////////////
+#include <FMCA/src/util/Errors.h>
+#include <FMCA/src/util/Tictoc.h>
+
+struct expKernel {
+  template <typename derived, typename otherDerived>
+  double operator()(const Eigen::MatrixBase<derived> &x,
+                    const Eigen::MatrixBase<otherDerived> &y) const {
+    return exp(-(x - y).norm());
+  }
+};
+
+using Interpolator = FMCA::TotalDegreeInterpolator<FMCA::FloatType>;
+using SampletInterpolator = FMCA::MonomialInterpolator<FMCA::FloatType>;
+using Moments = FMCA::NystromMoments<Interpolator>;
+using SampletMoments = FMCA::NystromSampletMoments<SampletInterpolator>;
+using MatrixEvaluator = FMCA::NystromMatrixEvaluator<Moments, expKernel>;
+using H2SampletTree = FMCA::H2SampletTree<FMCA::ClusterTree>;
+
+int main(int argc, char *argv[]) {
+  const unsigned int dim = atoi(argv[1]);
+  const unsigned int dtilde = 4;
+  const auto function = expKernel();
+  const double eta = 0.8;
+  const unsigned int mp_deg = 6;
+  const double threshold = 1e-5;
+  FMCA::Tictoc T;
+  std::fstream file;
+  file.open("s_output" + std::to_string(dim) + "_" + std::to_string(dtilde) +
+                "_EXPEXP.txt",
+            std::ios::out | std::ios::app);
+  file << "         m           n       nz(A)";
+  file << "         mem         err       time\n";
+  for (unsigned int npts : {1e3, 5e3, 1e4, 5e4, 1e5, 5e5, 1e6, 5e6}) {
+    // for (unsigned int npts : {5e6}) {
+    std::cout << "N:" << npts << " dim:" << dim << " eta:" << eta
+              << " mpd:" << mp_deg << " dt:" << dtilde
+              << " thres: " << threshold << std::endl;
+    T.tic();
+    const Eigen::MatrixXd P = Eigen::MatrixXd::Random(dim, npts);
+    T.toc("geometry generation: ");
+    const Moments mom(P, mp_deg);
+    const MatrixEvaluator mat_eval(mom, function);
+    const SampletMoments samp_mom(P, dtilde - 1);
+    T.tic();
+    const H2SampletTree hst(mom, samp_mom, 0, P);
+    T.toc("tree setup: ");
+    std::cout << std::flush;
+    FMCA::symmetric_compressor_impl<H2SampletTree> symComp;
+    T.tic();
+    symComp.compress(hst, mat_eval, eta, threshold);
+    const double tcomp = T.toc("symmetric compressor: ");
+    std::cout << std::flush;
+
+    {
+      Eigen::VectorXd x(npts), y1(npts), y2(npts);
+      double err = 0;
+      double nrm = 0;
+      const double tripSize = sizeof(Eigen::Triplet<double>);
+      const double nTrips = symComp.pattern_triplets().size();
+      file << std::setw(10) << std::setprecision(6) << npts << "\t";
+      file << std::setw(10) << std::setprecision(6) << npts << "\t";
+      file << std::setw(10) << std::setprecision(6) << std::ceil(nTrips / npts)
+           << "\t";
+      file << std::flush;
+      file << std::setw(10) << std::setprecision(5) << nTrips * tripSize / 1e9
+           << "\t";
+      std::cout << "nz(S): " << std::ceil(nTrips / npts) << std::endl;
+      std::cout << "memory: " << nTrips * tripSize / 1e9 << "GB\n"
+                << std::flush;
+      T.tic();
+      for (auto i = 0; i < 100; ++i) {
+        unsigned int index = rand() % P.cols();
+        x.setZero();
+        x(index) = 1;
+        y1 = FMCA::matrixColumnGetter(P, hst.indices(), function, index);
+        x = hst.sampletTransform(x);
+        y2.setZero();
+        for (const auto &i : symComp.pattern_triplets()) {
+          y2(i.row()) += i.value() * x(i.col());
+          if (i.row() != i.col()) y2(i.col()) += i.value() * x(i.row());
+        }
+        y2 = hst.inverseSampletTransform(y2);
+        err += (y1 - y2).squaredNorm();
+        nrm += y1.squaredNorm();
+      }
+      const double thet = T.toc("matrix vector time: ");
+      std::cout << thet / 100 << std::endl;
+      err = sqrt(err / nrm);
+      std::cout << "compression error: " << err << std::endl << std::flush;
+      file << std::setw(10) << std::setprecision(6) << err << "\t";
+      file << std::setw(10) << std::setprecision(6) << tcomp << "\n";
+      file << std::flush;
+    }
+    std::cout << std::string(60, '-') << std::endl;
+  }
+  file.close();
+  return 0;
+}
