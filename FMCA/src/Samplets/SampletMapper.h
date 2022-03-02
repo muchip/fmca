6defdccae4c94592caa131ea0b1b3af4774533e7
mmulterer <mmulterer@multemMini.local>
2022-03-02 17:34:17 +0100

    added samplet level mapper

--- FMCA/src/Samplets/SampletMapper.h
+++ FMCA/src/Samplets/SampletMapper.h
@@ -31,6 +31,23 @@ void sampletMapper(SampletTreeBase<Derived> &ST) {
   return;
 }
 
+template <typename Derived>
+std::vector<IndexType> sampletLevelMapper(SampletTreeBase<Derived> &ST) {
+  assert(ST.is_root() &&
+         "sampletLevelMapper needs to be called from the root cluster");
+  std::vector<IndexType> retval;
+  // assign vector start_index to each wavelet cluster
+  double geo_diam = ST.bb().col(2).norm();
+  for (auto &&it : ST) {
+    double diam = it.bb().col(2).norm();
+    int level = std::round(-log(diam / geo_diam) / log(2));
+    if (it.is_root())
+      for (auto i = 0; i < it.derived().nscalfs(); ++i) retval.push_back(level);
+    for (auto i = 0; i < it.derived().nsamplets(); ++i) retval.push_back(level);
+  }
+  return retval;
+}
+
 }  // namespace internal
 }  // namespace FMCA
 #endif
