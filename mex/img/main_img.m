19a1d52f2114f168ca903b907f386983aebe8bd1
mmulterer <mmulterer@multemMini.usilu.net>
2022-03-10 18:04:44 +0100

    update mex part

--- mex/img/main_img.m
+++ mex/img/main_img.m
@@ -1,24 +1,32 @@
 clear all;
 close all;
 addpath('../')
-ImgInput = imread('duck.jpg');
+ImgInput = imread('LuganoMuenster.png');
 Img = rgb2gray(ImgInput);
 y = [0:size(Img,1)-1] / size(Img,1);
 x = [0:size(Img,2)-1] / size(Img,2);
-[Ty, Iy, Ly] = MEXsampletBasis(y, 3);
+[Ty, Iy, Ly, Ry] = MEXsampletBasis(y, 5);
 Ty = sparse(Ty(:,1), Ty(:,2), Ty(:,3), length(y), length(y));
-[Tx, Ix, Lx] = MEXsampletBasis(x, 3);
+[Tx, Ix, Lx, Rx] = MEXsampletBasis(x, 5);
 max(Lx)
 max(Ly)
 Tx = sparse(Tx(:,1), Tx(:,2), Tx(:,3), length(x), length(x));
 figure(1)
-TImg = (Ty*Ty * double(Img) * Tx' * Tx);
-imin = min(min(TImg))
-imax = max(max(TImg))
-TImg = (TImg - imin) / (imax - imin);
-imin = min(min(TImg))
-imax = max(max(TImg))
-%TImg(find(Ly > 6), :) = 0;
-%TImg(:, find(Lx > 6)) = 0;
-%TImg = Ty' * TImg * Tx;
+lvlx = 6;
+lvly = 6;
+Txtrunc = Tx;
+Txtrunc(find(Lx > lvlx),:) = 0;
+Tytrunc = Ty;
+Tytrunc(find(Ly > lvly),:) = 0;
+TImg = (Tytrunc * double(Img) * Txtrunc');
+size(TImg)
+imshow(TImg);
+
+TImg(find(Ly > lvly), :) = 0;
+TImg(:, find(Lx > lvlx)) = 0;
+figure(1)
+%TImgC = mat2gray(TImg);
+TImg = Ty' * TImg * Tx;
+TImg = mat2gray(TImg);
+figure(2)
 imshow(TImg);
