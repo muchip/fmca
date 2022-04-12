814e380dfeb8e06b2a778ba91f2313cadd39bbaf
muchip <muchip@users.noreply.github.com>
2022-04-12 18:04:03 +0200

    fill distance

--- examples/stash/SwissCheese.cpp
+++ examples/stash/SwissCheese.cpp
@@ -51,57 +51,57 @@ double clusterSeparationRadius(double separation_radius,
 }
 
 template <typename Derived, typename Derived2>
-double pointDistance(double curr_distance, IndexType pt_index,
-                     const Derived &CT, const Eigen::MatrixBase<Derived2> &P) {
-  const auto mp = 0.5 * (CT.bb().col(1) - CT.bb().col(0));
-  const double radius = 0.5 * CT.bb().col(2).norm();
-  double dist = (P.col(pt_index) - mp).norm() - radius;
-  dist = dist > 0 ? dist : 0;
-
-  if (dist < curr_distance) {
+void clusterFillDistance(Eigen::VectorXd &fill_distance,
+                         const Derived &cluster1, const Derived &cluster2,
+                         const Eigen::MatrixBase<Derived2> &P) {
+  double max_dist = 0;
+  double dist = computeDistance(cluster1, cluster2);
+  // check cluster if there is the chance of improving the distance of a given
+  // point
+  if (fill_distance.maxCoeff() > dist) {
     if (cluster2.nSons()) {
-      rad = 0.5 * (dist + cluster2.bb().col(2).norm());
-      separation_radius = separation_radius < rad ? separation_radius : rad;
-      for (auto i = 0; i < cluster2.nSons(); ++i) {
-        rad = clusterSeparationRadius(separation_radius, cluster1,
-                                      cluster2.sons(i), P);
-        retval = retval < rad ? retval : rad;
-      }
+      for (auto i = 0; i < cluster2.nSons(); ++i)
+        clusterFillDistance(fill_distance, cluster1, cluster2.sons(i), P);
     } else {
       if (cluster1.block_id() != cluster2.block_id())
         for (auto j = 0; j < cluster1.indices().size(); ++j)
           for (auto i = 0; i < cluster2.indices().size(); ++i) {
-            rad = 0.5 *
-                  (P.col(cluster2.indices()[i]) - P.col(cluster1.indices()[j]))
-                      .norm();
-            retval = retval < rad ? retval : rad;
+            max_dist =
+                (P.col(cluster2.indices()[i]) - P.col(cluster1.indices()[j]))
+                    .norm();
+            fill_distance(j) =
+                fill_distance(j) > max_dist ? max_dist : fill_distance(j);
           }
     }
   }
-  return retval;
+  return;
 }
-
-
 
 int main() {
   FMCA::Tictoc T;
   // unsigned int npts = 1e5;`
   T.tic();
   Eigen::MatrixXd P =
-      Eigen::MatrixXd::Random(3, 200000);  // = generateSwissCheeseExp(2, npts);
+      Eigen::MatrixXd::Random(3, 80000); // = generateSwissCheeseExp(2, npts);
   double separation_radius_full = double(1.) / double(0.);
-#if 1
+  double fill_distance_full = 0;
   T.tic();
-  for (auto j = 0; j < P.cols(); ++j)
-    for (auto i = j + 1; i < P.cols(); ++i) {
-      double rad = 0.5 * (P.col(i) - P.col(j)).norm();
-      separation_radius_full =
-          separation_radius_full < rad ? separation_radius_full : rad;
+  for (auto j = 0; j < P.cols(); ++j) {
+    double dist = double(1.) / double(0.);
+    for (auto i = 0; i < P.cols(); ++i) {
+      if (i != j) {
+        double rad = 0.5 * (P.col(i) - P.col(j)).norm();
+        dist = dist > (2 * rad) ? (2 * rad) : dist;
+        separation_radius_full =
+            separation_radius_full < rad ? separation_radius_full : rad;
+      }
     }
+    fill_distance_full = fill_distance_full < dist ? dist : fill_distance_full;
+  }
   T.toc("time full rad: ");
   std::cout << "rad full distance matrix: " << separation_radius_full
             << std::endl;
-#endif
+  std::cout << "fill full distance matrix: " << fill_distance_full << std::endl;
   // Eigen::MatrixXd Pts = readMatrix("../../Points/cross3D.txt");
   // P = Pts.transpose();
   // Bembel::IO::print2m("d1c.m", "P", P, "w");
@@ -111,7 +111,7 @@ int main() {
   // Q.setZero();
   // Q.topRows(2) = P;
   Q = P;
-  FMCA::ClusterTree CT(Q, 2);
+  FMCA::ClusterTree CT(Q, 10);
   T.tic();
   double separation_radius = double(1.) / double(0.);
   for (auto &it : CT) {
@@ -131,37 +131,47 @@ int main() {
   std::cout << "err: " << std::abs(separation_radius - separation_radius_full)
             << std::endl;
   double fill_distance = double(0.);
+  Eigen::VectorXd min_distance;
   double dist = 0;
-  for (auto i = 0; i < P.cols(); ++i) {
-    const auto cluster =
-        pointMinDistance(P.col(i), P, CT, separation_radius, &dist);
+  for (auto &it : CT) {
     if (!it.nSons()) {
-      double rad = 0;
+      min_distance.resize(it.indices().size());
+      min_distance.setOnes();
+      min_distance /= double(0.);
+      // determine candidate distances within the cluster
       for (auto j = 0; j < it.indices().size(); ++j)
-        for (auto i = j + 1; i < it.indices().size(); ++i) {
-          dist = (P.col(it.indices()[i]) - P.col(it.indices()[j])).norm();
-          fill_distance = separation_radius < rad ? separation_radius : rad;
+        for (auto i = 0; i < it.indices().size(); ++i) {
+          if (i != j) {
+            dist = (P.col(it.indices()[i]) - P.col(it.indices()[j])).norm();
+            min_distance(j) = min_distance(j) > dist ? dist : min_distance(j);
+          }
         }
-      rad = clusterSeparationRadius(separation_radius, it, CT, P);
-      separation_radius = separation_radius < rad ? separation_radius : rad;
+      clusterFillDistance(min_distance, it, CT, P);
+      const double cluster_max_dist = min_distance.maxCoeff();
+      fill_distance =
+          fill_distance < cluster_max_dist ? cluster_max_dist : fill_distance;
     }
   }
   T.toc("fill distance comp: ");
   std::cout << fill_distance << std::endl;
+  std::cout << "error: " << std::abs(fill_distance - fill_distance_full)
+            << std::endl;
 
   T.tic();
   std::vector<const FMCA::TreeBase<FMCA::ClusterTree> *> leafs;
   for (auto level = 0; level < 16; ++level) {
     std::vector<Eigen::MatrixXd> bbvec;
     for (auto &node : CT) {
-      if (node.level() == level) bbvec.push_back(node.derived().bb());
+      if (node.level() == level)
+        bbvec.push_back(node.derived().bb());
     }
     FMCA::IO::plotBoxes("boxes" + std::to_string(level) + ".vtk", bbvec);
   }
   std::vector<Eigen::MatrixXd> bbvec;
 
   for (auto &node : CT) {
-    if (!node.nSons()) bbvec.push_back(node.derived().bb());
+    if (!node.nSons())
+      bbvec.push_back(node.derived().bb());
   }
   Eigen::VectorXd colrs(P.cols());
   for (auto &node : CT) {
