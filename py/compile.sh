c626de9acfb38f25492f8641800201779f380a77
mmulterer <mmulterer@multemMini.usilu.net>
2022-06-14 19:59:10 +0200

    hope to find the performance issue

--- py/compile.sh
+++ py/compile.sh
@@ -1,4 +1,4 @@
-g++-11 -g -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) \
+g++-11 -O3 -fopenmp -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) \
  -I/opt/homebrew/include/eigen3 -I../ \
 FMCA.cpp -o FMCA$(python3-config --extension-suffix) \
 -L/opt/homebrew/Cellar/python@3.9/3.9.10/\
