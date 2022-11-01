e93566e2691ecfbd8e9d4ab2888a8d5226acf1e8
mmulterer <mmulterer@multemMini.usilu.net>
2022-11-01 10:20:19 +0100

    added numerical limits

--- FMCA/src/util/Macros.h
+++ FMCA/src/util/Macros.h
@@ -12,18 +12,26 @@
 #ifndef FMCA_UTIL_MACROS_H_
 #define FMCA_UTIL_MACROS_H_
 
+#include <Eigen/Dense>
+#include <limits>
+
 namespace FMCA {
 #ifndef M_PI
 #define FMCA_PI 3.14159265358979323846264338327950288
 #else
 #define FMCA_PI M_PI
 #endif
-#define FMCA_BBOX_THREASHOLD 1e-2
-#define FMCA_ZERO_TOLERANCE 2e-16
 
-typedef unsigned int Index;
+// define primitive types used throughout the toolbox
+#define FMCA_INDEX unsigned int
+#define FMCA_SCALAR double
+#define FMCA_INF std::numeric_limits<FMCA_SCALAR>::infinity()
+#define FMCA_ZERO_TOLERANCE std::numeric_limits<FMCA_SCALAR>::epsilon()
+#define FMCA_BBOX_THREASHOLD FMCA_ZERO_TOLERANCE
 
-typedef double Scalar;
+typedef FMCA_INDEX Index;
+
+typedef FMCA_SCALAR Scalar;
 
 typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
 
@@ -32,6 +40,6 @@ typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1u> Vector;
 typedef Eigen::Matrix<Index, Eigen::Dynamic, Eigen::Dynamic> iMatrix;
 
 typedef Eigen::Matrix<Index, Eigen::Dynamic, 1u> iVector;
-} // namespace FMCA
+}  // namespace FMCA
 
 #endif
