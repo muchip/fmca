d5d49e82863743e246d4763ff6db89507a26d4d4
mmulterer <mmulterer@multemMBP.usilu.net>
2022-12-01 17:33:58 +0100

    adds kNN including tests

--- FMCA/src/Clustering/ClusterTreeMetrics.h
+++ FMCA/src/Clustering/ClusterTreeMetrics.h
@@ -14,7 +14,6 @@
 
 #include <memory>
 
-#include "../util/KMinList.h"
 #include "../util/Tictoc.h"
 
 namespace FMCA {
@@ -136,85 +135,6 @@ Vector minDistanceVector(const ClusterTreeBase<Derived> &CT, const Matrix &P) {
     }
   }
   return min_distance;
-}
-
-template <typename Derived>
-void updateClusterKMinDistance(std::vector<KMinList> &min_dist,
-                               Scalar max_min_dist,
-                               const ClusterTreeBase<Derived> &c1,
-                               const ClusterTreeBase<Derived> &c2,
-                               const Matrix &P) {
-  Scalar dist = computeDistance(c1, c2);
-  if (max_min_dist >= dist) {
-    if (c2.nSons()) {
-      dist += c2.bb().col(2).norm();
-      // max_min_dist = max_min_dist < dist ? max_min_dist : dist;
-      for (Index i = 0; i < c2.nSons(); ++i)
-        if (c2.sons(i).indices().size())
-          updateClusterKMinDistance(min_dist, max_min_dist, c1, c2.sons(i), P);
-    } else {
-      if (c1.block_id() != c2.block_id())
-        for (Index j = 0; j < c1.indices().size(); ++j)
-          for (Index i = 0; i < c2.indices().size(); ++i) {
-            dist = (P.col(c2.indices()[i]) - P.col(c1.indices()[j])).norm();
-            min_dist[c2.indices()[i]].insert(
-                std::make_pair(c1.indices()[j], dist));
-            min_dist[c1.indices()[j]].insert(
-                std::make_pair(c2.indices()[i], dist));
-          }
-      // determine max_min_distance within cluster
-      max_min_dist = 0;
-      for (Index j = 0; j < c1.indices().size(); ++j) {
-        const Scalar dist = min_dist[c1.indices()[j]].max_min();
-        max_min_dist = max_min_dist < dist ? dist : max_min_dist;
-      }
-    }
-  }
-  return;
-}
-
-template <typename Derived>
-iMatrix kMinDistance(const ClusterTreeBase<Derived> &CT, const Matrix &P,
-                     const Index k = 1) {
-  assert(k < P.cols() && "too few data points for kmin");
-  iMatrix kmin_distance(P.cols(), k);
-  std::vector<KMinList> qvec(P.cols());
-  Scalar max_min_distance = 0;
-#pragma omp parallel for
-  for (Index i = 0; i < qvec.size(); ++i) {
-    qvec[i] = KMinList(k);
-    for (Index j = 1; j <= k; ++j) {
-      const Index idx = (i + j) % P.cols();
-      assert(idx != i && "should never happen");
-      const Scalar dist = (P.col(i) - P.col(idx)).norm();
-      qvec[i].insert(std::make_pair(idx, dist));
-    }
-  }
-  // compute min_distance at the leafs
-  for (auto it = CT.cbegin(); it != CT.cend(); ++it) {
-    if (!it->nSons() && it->indices().size()) {
-      const std::vector<Index> &idcs = it->indices();
-      for (Index j = 0; j < idcs.size(); ++j)
-        for (Index i = 0; i < j; ++i) {
-          const Scalar dist = (P.col(idcs[i]) - P.col(idcs[j])).norm();
-          qvec[idcs[i]].insert(std::make_pair(idcs[j], dist));
-          qvec[idcs[j]].insert(std::make_pair(idcs[i], dist));
-        }
-      // determine max_min_distance within cluster
-      max_min_distance = 0;
-      for (Index j = 0; j < idcs.size(); ++j) {
-        const Scalar dist = qvec[idcs[j]].max_min();
-        max_min_distance = max_min_distance < dist ? dist : max_min_distance;
-      }
-      assert(max_min_distance > 0 && "min_dist should be positive");
-      updateClusterKMinDistance(qvec, max_min_distance, *it, CT, P);
-    }
-  }
-  for (Index i = 0; i < qvec.size(); ++i) {
-    Index j = 0;
-    for (const auto &it : qvec[i].list()) kmin_distance(i, j++) = it.first;
-  }
-  return kmin_distance;
 }
 
 template <typename Derived>
---
+++ tests/test_kNN.cpp
@@ -0,0 +1,71 @@
+// This file is part of FMCA, the Fast Multiresolution Covariance Analysis
+// package.
+//
+// Copyright (c) 2022, Michael Multerer
+//
+// All rights reserved.
+//
+// This source code is subject to the GNU Affero General Public License v3.0
+// license and without any warranty, see <https://github.com/muchip/FMCA>
+// for further information.
+//
+#include <Eigen/Dense>
+#include <fstream>
+#include <iostream>
+
+#include "../FMCA/Clustering"
+#include "IO.h"
+
+template <typename Dists>
+struct my_less {
+  my_less(const Dists &dist) : dist_(dist) {}
+  template <typename idx>
+  bool operator()(const idx &a, const idx &b) const {
+    return dist_(a) < dist_(b);
+  }
+  const Dists &dist_;
+};
+
+#define DIM 3
+#define KMINS 10
+
+int main(int argc, char *argv[]) {
+  FMCA::Tictoc T;
+  const FMCA::Index leaf_size = 10;
+  for (FMCA::Index tests = 0; tests < 4; ++tests) {
+    FMCA::Scalar told = 1;
+    FMCA::Scalar tnew = 2;
+    FMCA::Scalar avgfac = 0;
+    for (FMCA::Index lvl = 4; lvl < 20; ++lvl) {
+      const FMCA::Index npts = 1 << lvl;
+      std::cout << npts << "\t " << lvl << "\t ";
+      FMCA::Matrix P = FMCA::Matrix::Random(DIM, npts);
+      FMCA::ClusterTree CT(P, leaf_size);
+      T.tic();
+      FMCA::iMatrix kMins = kNN(CT, P, KMINS);
+      tnew = T.toc();
+      std::cout << tnew << "sec.\t factor:" << tnew / told << std::endl;
+      if (lvl > 4) avgfac += tnew / told;
+      told = tnew;
+#pragma omp parallel for
+      for (auto j = 0; j < 100; ++j) {
+        FMCA::Index the_index = rand() % P.cols();
+        FMCA::Vector dist(P.cols());
+        std::vector<FMCA::Index> idx(P.cols());
+        std::iota(idx.begin(), idx.end(), 0);
+        for (auto i = 0; i < P.cols(); ++i)
+          dist(i) = (P.col(i) - P.col(the_index)).norm();
+        std::sort(idx.begin(), idx.end(), my_less<FMCA::Vector>(dist));
+        for (auto i = 1; i <= KMINS; ++i) {
+          const FMCA::Scalar the_dist =
+              (P.col(the_index) - P.col(kMins(the_index, i - 1))).norm();
+          // assert(idx[i] == kMins(j, i - 1) && "index mismatch");
+          assert(std::abs(the_dist - dist(idx[i])) < FMCA_ZERO_TOLERANCE &&
+                 "dist mismatch");
+        }
+      }
+    }
+    std::cout << "average factor in this run: " << avgfac / 15 << std::endl;
+  }
+  return 0;
+}
