d86677485dbd19cb2e9e39ca61af5f04668980fc
mmulterer <mmulterer@multemMini.usilu.net>
2023-07-28 14:49:37 +0200

    adds wtdmp

--- FMCA/src/Interpolator/DeMarchiPoints.h
+++ FMCA/src/Interpolator/DeMarchiPoints.h
@@ -9,31 +9,48 @@
 // license and without any warranty, see <https://github.com/muchip/FMCA>
 // for further information.
 //
-#ifndef FMCA_INTERPOLATORS_DEMARCHIPOINTS_H_
-#define FMCA_INTERPOLATORS_DEMARCHIPOINTS_H_
+#ifndef FMCA_INTERPOLATOR_DEMARCHIPOINTS_H_
+#define FMCA_INTERPOLATOR_DEMARCHIPOINTS_H_
+
+#include <Eigen/QR>
+#include <Eigen/SVD>
 
 #include "../util/HaltonSet.h"
+#include "evalPolynomials.h"
 
 namespace FMCA {
 /**
  *  \brief These are DeMarchi points on [0,1]^d
  **/
 template <typename MultiIndexSet>
-Matrix DeMarchiPoints(const MultiIndexSet &idcs, Index oversmplng_factor = 10) {
+Matrix DeMarchiPoints(const MultiIndexSet &idcs,
+                      const Index oversmplng_factor = 10) {
   const unsigned int dim = idcs.dim();
   const unsigned int n = idcs.index_set().size();
   Matrix retval(dim, n);
-  Matrix VT(n, oversampl_factor * n);
+  Matrix VT(n, oversmplng_factor * n);
   // create matrix of Halton points
-  Eigen::MatrixXd Halton_pts(dim, oversampl_factor * n);
+  Matrix Halton_pts(dim, oversmplng_factor * n);
   HaltonSet<100> hs(dim);
   for (Index i = 0; i < Halton_pts.cols(); ++i) {
     Halton_pts.col(i) = hs.EigenHaltonVector();
     hs.next();
   }
   for (Index i = 0; i < Halton_pts.cols(); ++i)
-    retval.col(i) = evalPolynomials(pts.col(i), idcs).transpose();
-
+    VT.col(i) = internal::evalPolynomials(idcs, Halton_pts.col(i));
+  Eigen::ColPivHouseholderQR<Matrix> qr;
+  qr.compute(VT);
+  const auto &pt_idcs = qr.colsPermutation().indices();
+  for (Index i = 0; i < n; ++i) retval.col(i) = Halton_pts.col(pt_idcs(i));
+  // compute condition of interpolation
+  VT.resize(n, n);
+  for (Index i = 0; i < retval.cols(); ++i)
+    VT.col(i) = internal::evalPolynomials(idcs, retval.col(i));
+  Eigen::JacobiSVD<Matrix> svd;
+  svd.compute(VT);
+  std::cout << "DeMarchi points condition: "
+            << svd.singularValues().maxCoeff() / svd.singularValues().minCoeff()
+            << std::endl;
   return retval;
 }
 
