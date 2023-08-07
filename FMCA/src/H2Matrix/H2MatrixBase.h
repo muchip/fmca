79cbd73d82e204f93ed0f152b79f93b689bb1cfd
MM <mm@mm.me>
2023-08-07 11:01:29 +0200

    pulls updated H2MatrixBase from FMM

--- FMCA/src/H2Matrix/H2MatrixBase.h
+++ FMCA/src/H2Matrix/H2MatrixBase.h
@@ -71,17 +71,23 @@ struct H2MatrixBase : TreeBase<Derived> {
   // (m, n, fblocks, lrblocks, nz(A), mem)
   Matrix statistics() const {
     Matrix retval(6, 1);
-    Index lr_blocks = 0;
-    Index f_blocks = 0;
-    Index mem = 0;
+    size_t lr_blocks = 0;
+    size_t f_blocks = 0;
+    size_t mem = 0;
     assert(is_root() && "statistics needs to be called from root");
     for (auto &&it : *this) {
+      const RowCType &row = *(it.rcluster());
+      const ColCType &col = *(it.ccluster());
       if (!it.nSons()) {
-        if (it.is_low_rank())
+        if (it.is_low_rank()) {
+          size_t block_rows = row.nSons() ? row.Es()[0].rows() : row.V().rows();
+          size_t block_cols = col.nSons() ? col.Es()[0].rows() : col.V().rows();
           ++lr_blocks;
-        else
+          mem += block_rows * block_cols;
+        } else {
           ++f_blocks;
-        mem += it.node().S_.size();
+          mem += row.indices().size() * row.indices().size();
+        }
       }
     }
     retval(0, 0) = rows();
