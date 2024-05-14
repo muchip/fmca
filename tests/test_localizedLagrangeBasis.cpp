044a4eae4d23eb7ee14af5d87369cdea847d8961
mmulterer <mmulterer@multemMini.usilu.net>
2024-05-14 14:51:24 +0200

    updates test

--- tests/test_localizedLagrangeBasis.cpp
+++ tests/test_localizedLagrangeBasis.cpp
@@ -9,14 +9,22 @@
 // license and without any warranty, see <https://github.com/muchip/FMCA>
 // for further information.
 //
-#include <Eigen/Dense>
 #include <fstream>
 #include <iostream>
+//
+#include <Eigen/Dense>
+#include <Eigen/MetisSupport>
+#include <Eigen/Sparse>
+#include <Eigen/SparseCholesky>
+using EigenCholesky =
+    Eigen::SimplicialLLT<Eigen::SparseMatrix<double>, Eigen::Upper,
+                         Eigen::MetisOrdering<int>>;
 
 #include "../FMCA/Clustering"
 #include "../FMCA/CovarianceKernel"
 #include "../FMCA/H2Matrix"
 #include "../FMCA/src/Clustering/epsNN.h"
+#include "../FMCA/src/H2Matrix/H2MatrixProjector.h"
 #include "../FMCA/src/util/Tictoc.h"
 
 using Interpolator = FMCA::TotalDegreeInterpolator;
@@ -27,13 +35,12 @@ using H2Matrix = FMCA::H2Matrix<H2ClusterTree>;
 
 int main(int argc, char *argv[]) {
   FMCA::Tictoc T;
-  const FMCA::Index npts = 20000;
-  const FMCA::Index dim = 3;
-  const FMCA::Index leaf_size = 100;
+  const FMCA::Index npts = 2000;
+  const FMCA::Index dim = 2;
   const FMCA::Index K = 2;
-  const FMCA::Index mpole_deg = 4;
+  const FMCA::Index mpole_deg = 5;
   const FMCA::Scalar eta = 1. / dim;
-  const FMCA::CovarianceKernel kernel("EXPONENTIAL", 1.);
+  const FMCA::CovarianceKernel kernel("EXPONENTIAL", .1);
   const FMCA::Matrix P = FMCA::Matrix::Random(dim, npts);
 
   T.tic();
@@ -89,6 +96,39 @@ int main(int argc, char *argv[]) {
   invK.setFromTriplets(triplets.begin(), triplets.end());
   std::cout << "anz:                          " << triplets.size() / npts
             << std::endl;
+  triplets.clear();
+  T.tic();
+#pragma omp parallel for
+  for (FMCA::Index i = 0; i < epsnn.size(); ++i) {
+    const FMCA::Index locN = epsnn[i].size();
+    std::vector<Eigen::Triplet<FMCA::Scalar>> local_triplets;
+    for (FMCA::Index j = 0; j < locN; ++j) {
+      const FMCA::Index row_id = inv_idcs[i];
+      const FMCA::Index col_id = inv_idcs[epsnn[i][j]];
+      const FMCA::Scalar val = kernel.eval(P.col(i), P.col(epsnn[i][j]))(0, 0);
+      if (row_id <= col_id)
+        local_triplets.push_back(
+            Eigen::Triplet<FMCA::Scalar>(row_id, col_id, val));
+    }
+#pragma omp critical
+    triplets.insert(triplets.end(), local_triplets.begin(),
+                    local_triplets.end());
+  }
+  T.toc("local kernel triplets:       ");
+  Eigen::SparseMatrix<FMCA::Scalar> Ksparse(npts, npts);
+  Ksparse.setFromTriplets(triplets.begin(), triplets.end());
+  std::cout << "anz:                          " << triplets.size() / npts
+            << std::endl;
+  T.tic();
+  EigenCholesky solver;
+  std::cout << Ksparse.norm() << std::endl;
+  std::cout << Ksparse.diagonal().minCoeff() << std::endl;
+  solver.compute(Ksparse);
+   T.toc("time factorization: ");
+  std::cout << "sinfo: " << (solver.info() == Eigen::Success) << std::endl;
+  std::cout << "nz(L): "
+            << solver.matrixL().nestedExpression().nonZeros() / P.cols()
+            << std::endl;
   {
     FMCA::Matrix X(npts, 10), Y1(npts, 10), Y2(npts, 10);
     X.setZero();
@@ -124,12 +164,29 @@ int main(int argc, char *argv[]) {
     std::cout << std::string(60, '-') << std::endl;
   }
   hmat.statistics();
+  T.tic();
+  FMCA::Matrix H2K = hmat.full();
+  FMCA::Matrix invH2K = H2K.inverse();
+  FMCA::H2MatrixProjector<H2Matrix> proj(hmat);
+  H2Matrix invhmat = proj.project(invH2K);
+  H2Matrix test = proj.project(H2K);
 
+  T.toc("inverted full H2 matrix and projected:");
   FMCA::Matrix X(npts, 10), Y1(npts, 10), Y2(npts, 10);
   X.setRandom();
-  Y1 = hmat * X;
+  Y1 = Ksparse * X;
   Y2 = invK * Y1;
   std::cout << "inverse error vs H2-Matrix:   " << (Y2 - X).norm() / X.norm()
+            << std::endl;
+  X.setRandom();
+  Y1 = hmat * X;
+  Y2 = invhmat * Y1;
+  std::cout << "inverse H2-error:             " << (Y2 - X).norm() / X.norm()
+            << std::endl;
+  X.setRandom();
+  Y1 = hmat * X;
+  Y2 = test * X;
+  std::cout << "H2-projector error:           " << (Y2 - Y1).norm() / Y1.norm()
             << std::endl;
   return 0;
 }
