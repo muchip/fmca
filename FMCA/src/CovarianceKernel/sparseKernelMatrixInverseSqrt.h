39b4b1395d221bf100ef91e02f9e817f99495156
mmulterer <mmulterer@multemMBP.usilu.net>
2024-06-18 09:21:59 +0200

    adds sparse sqrt and cleans up tests

---
+++ FMCA/src/CovarianceKernel/sparseKernelMatrixInverseSqrt.h
@@ -0,0 +1,76 @@
+// This file is part of FMCA, the Fast Multiresolution Covariance Analysis
+// package.
+//
+// Copyright (c) 2022, Michael Multerer
+//
+// All rights reserved.
+//
+// This source code is subject to the GNU Affero General Public License v3.0
+// license and without any warranty, see <https://github.com/muchip/FMCA>
+// for further information.
+//
+#ifndef FMCA_COVARIANCEKERNEL_SPARSEKERNELMATRIXINVERSESQRT_H_
+#define FMCA_COVARIANCEKERNEL_SPARSEKERNELMATRIXINVERSESQRT_H_
+
+namespace FMCA {
+
+template <typename Derived>
+Eigen::SparseMatrix<Scalar> sparseKernelMatrixInverseSqrt(
+    const CovarianceKernel &K, const ClusterTreeBase<Derived> &CT,
+    const Matrix &P, const Index fps = 1, const Scalar ridge_parameter = 0,
+    const Scalar zeroTol = FMCA_ZERO_TOLERANCE) {
+  const Vector mdv = minDistanceVector(CT, P);
+  const Scalar fill_distance = mdv.maxCoeff();
+  const Scalar search_radius =
+      fps * fill_distance * std::abs(std::log(fill_distance));
+  std::cout << std::string(28, '-') << "sparseInverseSqrt"
+            << std::string(28, '-') << std::endl;
+  std::cout << "fill distance:                " << fill_distance << std::endl;
+  std::cout << "search radius:                " << search_radius << std::endl;
+  // compute epsilon nearest neighbours for all points
+  std::vector<std::vector<Index>> epsnn(P.cols());
+#pragma omp parallel for
+  for (Index i = 0; i < epsnn.size(); ++i)
+    epsnn[i] = epsNN(CT, P, P.col(i), search_radius);
+  // evaluate localized inverse
+  std::vector<Eigen::Triplet<Scalar>> triplets;
+  // compute permutation from original order to cluster order
+  std::vector<Index> inv_idcs(P.cols());
+  for (FMCA::Index i = 0; i < inv_idcs.size(); ++i)
+    inv_idcs[CT.indices()[i]] = i;
+    // actually compute the localized inverse
+#pragma omp parallel for
+  for (FMCA::Index i = 0; i < epsnn.size(); ++i) {
+    const FMCA::Index locN = epsnn[i].size();
+    FMCA::Matrix Ploc(P.rows(), locN);
+    FMCA::Index pos = 0;
+    for (FMCA::Index j = 0; j < locN; ++j) {
+      Ploc.col(j) = P.col(epsnn[i][j]);
+      pos = epsnn[i][j] == i ? j : pos;
+    }
+    FMCA::Matrix Kloc = K.eval(Ploc, Ploc);
+    Kloc.diagonal().array() += ridge_parameter;
+    Eigen::SelfAdjointEigenSolver<FMCA::Matrix> es;
+    es.compute(Kloc, Eigen::ComputeEigenvectors);
+    FMCA::Vector diag(locN);
+    for (FMCA::Index i = 0; i < locN; ++i)
+      diag(i) = es.eigenvalues()(i) > zeroTol
+                    ? 1. / std::sqrt(es.eigenvalues()(i))
+                    : 0;
+    FMCA::Matrix invSqrt =
+        es.eigenvectors() * diag.asDiagonal() * es.eigenvectors().transpose();
+    FMCA::Vector col = invSqrt.col(pos);
+    std::vector<Eigen::Triplet<FMCA::Scalar>> local_triplets;
+    for (FMCA::Index j = 0; j < locN; ++j)
+      local_triplets.push_back(Eigen::Triplet<FMCA::Scalar>(
+          inv_idcs[i], inv_idcs[epsnn[i][j]], col(j)));
+#pragma omp critical
+    triplets.insert(triplets.end(), local_triplets.begin(),
+                    local_triplets.end());
+  }
+  Eigen::SparseMatrix<FMCA::Scalar> invsqrtK(P.cols(), P.cols());
+  invsqrtK.setFromTriplets(triplets.begin(), triplets.end());
+  return invsqrtK;
+}
+}  // namespace FMCA
+#endif
---
+++ tests/test_sparseKernelMatrixInverse.cpp
@@ -0,0 +1,59 @@
+// This file is part of FMCA, the Fast Multiresolution Covariance Analysis
+// package.
+//
+// Copyright (c) 2024, Michael Multerer
+//
+// All rights reserved.
+//
+// This source code is subject to the GNU Affero General Public License v3.0
+// license and without any warranty, see <https://github.com/muchip/FMCA>
+// for further information.
+//
+#include <fstream>
+#include <iostream>
+//
+#include <Eigen/Dense>
+
+#include "../FMCA/Clustering"
+#include "../FMCA/CovarianceKernel"
+#include "../FMCA/src/util/Tictoc.h"
+
+int main(int argc, char *argv[]) {
+  FMCA::Tictoc T;
+  const FMCA::Index npts = 1000;
+  const FMCA::Index dim = 2;
+  const FMCA::Index K = 2;
+  const FMCA::Scalar ridge_parameter = 0 * npts;
+  const FMCA::CovarianceKernel kernel("MaternNu", .1, 1., 0.5);
+  FMCA::Matrix P = FMCA::Matrix::Random(dim, npts);
+  std::cout << std::string(72, '-') << std::endl;
+  T.tic();
+  const FMCA::ClusterTree CT(P, 10);
+  std::cout
+      << "Cluster splitter:             "
+      << FMCA::internal::traits<FMCA::ClusterTree>::Splitter::splitterName()
+      << std::endl;
+  T.toc("cluster tree:                ");
+  T.tic();
+  Eigen::SparseMatrix<FMCA::Scalar> invK =
+      FMCA::sparseKernelMatrixInverse(kernel, CT, P, K, ridge_parameter);
+  T.toc("sparse inverse:              ");
+  std::cout << "anz:                          " << invK.nonZeros() / npts
+            << std::endl;
+  {
+    FMCA::Matrix X(npts, 100), Y1(npts, 100), Y2(npts, 100);
+    X.setZero();
+    for (auto i = 0; i < 100; ++i) {
+      FMCA::Index index = rand() % P.cols();
+      FMCA::Vector col = kernel.eval(P, P.col(CT.indices()[index]));
+      Y1.col(i) =
+          col(Eigen::Map<const FMCA::iVector>(CT.indices(), CT.block_size()));
+      X(index, i) = 1;
+    }
+    Y2 = invK * (Y1 + ridge_parameter * X);
+    FMCA::Scalar err = (Y2 - X).norm() / X.norm();
+    std::cout << "inverse error:                " << err << std::endl;
+    std::cout << std::string(72, '-') << std::endl;
+  }
+  return 0;
+}
