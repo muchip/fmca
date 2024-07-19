2fc60f3ea82f63976f98283edf91e827732f759a
SARA AVESANI <sara.avesani_02@studenti.univr.it>
2024-07-19 11:07:05 +0200

    Collocation not compressed -- full matrices

--- TestPDE/test_collocation_full.cpp
+++ TestPDE/test_collocation_full.cpp
@@ -9,15 +9,9 @@ We rely on the FMCA library by M.Multerer.
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
-#include <random>
 /////////////////////////////////////////////////
-#include </opt/homebrew/Cellar/metis/5.1.0/include/metis.h>
-
-#include <Eigen/Dense>
-
 #include "../FMCA/GradKernel"
 #include "../FMCA/H2Matrix"
-#include "../FMCA/LaplacianKernel"
 #include "../FMCA/Samplets"
 #include "../FMCA/src/FormattedMultiplication/FormattedMultiplication.h"
 #include "../FMCA/src/util/Grid2D.h"
@@ -49,29 +43,17 @@ int main() {
   FMCA::Matrix P_interior_full;
   FMCA::Matrix P_bnd_full;
 
-  /*
-    int N_interior = 100000;
-    FMCA::Matrix P_interior = MonteCarloPointsInterior(N_interior);
-    saveMatrixToFile(P_interior.transpose(),
-    "MC_Interior_Circle_collocation.txt"); int N_bnd = 50000; FMCA::Matrix P_bnd
-    = MonteCarloPointsBoundary(N_bnd); saveMatrixToFile(P_bnd.transpose(),
-    "MC_Bnd_Circle_collocation.txt");
-  */
+  readTXT("data/MC_Interior_Sphere_collocation.txt", P_interior_full, DIM);
+  readTXT("data/MC_Bnd_Sphere_collocation.txt", P_bnd_full, DIM);
 
-  readTXT("data/MC_Interior_Circle_collocation.txt", P_interior_full, DIM);
-  readTXT("data/MC_Bnd_Circle_collocation.txt", P_bnd_full, DIM);
-
-  int N_sources = 20;
-  int N_sources_border = 10;
+  int N_sources = 7000;
+  int N_sources_border = 3000;
   FMCA::Matrix P_sources = P_interior_full.leftCols(N_sources);
   FMCA::Matrix P_sources_border = P_bnd_full.leftCols(N_sources_border);
 
   FMCA::Matrix P(DIM, N_sources + N_sources_border);
   P << P_sources, P_sources_border;
   FMCA::IO::plotPoints("P_sphere.vtk", P);
- 
- // 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.5, 2.8, 3, 3.2, 3.5, 3.8, 4, 
-  std::vector<double> sigma_values = {4};
 
   FMCA::Matrix Peval = P;
 
@@ -106,94 +88,68 @@ int main() {
   std::string kernel_type = "MATERN52";
   std::string kernel_type_laplacian = "MATERN52_SECOND_DERIVATIVE";
   //////////////////////////////////////////////////////////////////////////////
-  for (double sigma : sigma_values) {
-    std::cout << std::string(100, '-') << std::endl;
-    std::cout << "Running with sigma = " << sigma << std::endl;
-    const Moments mom_sources(P_sources, MPOLE_DEG);
-    const SampletMoments samp_mom_sources(P_sources, dtilde - 1);
-    H2SampletTree hst_sources(mom_sources, samp_mom_sources, 0, P_sources);
-    const Moments mom_border(P_sources_border, MPOLE_DEG);
-    const SampletMoments samp_mom_border(P_sources_border, dtilde - 1);
-    H2SampletTree hst_sources_border(mom_border, samp_mom_border, 0,
-                                     P_sources_border);
-    const Moments mom(P, MPOLE_DEG);
-    const SampletMoments samp_mom(P, dtilde - 1);
-    H2SampletTree hst(mom, samp_mom, 0, P);
-    FMCA::Vector minDistance = minDistanceVector(hst, P);
-    auto maxElementIterator =
-        std::max_element(minDistance.begin(), minDistance.end());
-    FMCA::Scalar sigma_h = *maxElementIterator;
-    std::cout << "fill distance:                      " << sigma_h << std::endl;
-    //////////////////////////////////////////////////////////////////////////////
-    std::cout << std::string(80, '-') << std::endl;
-    std::cout << "Number of interior points = " << NPTS_SOURCE << std::endl;
-    std::cout << "Number of border points = " << NPTS_SOURCE_BORDER
-              << std::endl;
-    std::cout << "Total number of points = " << N << std::endl;
-    std::cout << "sigma = " << sigma << std::endl;
-    //////////////////////////////////////////////////////////////////////////////
-    const FMCA::CovarianceKernel kernel_funtion_border(kernel_type, sigma);
-    FMCA::Matrix K_bnd_n = kernel_funtion_border.eval(P_sources_border, P);
+  const Moments mom_sources(P_sources, MPOLE_DEG);
+  const SampletMoments samp_mom_sources(P_sources, dtilde - 1);
+  H2SampletTree hst_sources(mom_sources, samp_mom_sources, 0, P_sources);
+  const Moments mom_border(P_sources_border, MPOLE_DEG);
+  const SampletMoments samp_mom_border(P_sources_border, dtilde - 1);
+  H2SampletTree hst_sources_border(mom_border, samp_mom_border, 0,
+                                   P_sources_border);
+  const Moments mom(P, MPOLE_DEG);
+  const SampletMoments samp_mom(P, dtilde - 1);
+  H2SampletTree hst(mom, samp_mom, 0, P);
+  FMCA::Vector minDistance = minDistanceVector(hst, P);
+  FMCA::Scalar sigma_h = minDistance.mean();
+  std::cout << "average distance:                   " << sigma_h << std::endl;
 
-    //////////////////////////////////////////////////////////////////////////////
-    FMCA::Matrix Laplacian(NPTS_SOURCE, N);
-    Laplacian.setZero();
-
-    for (int i = 0; i < DIM; ++i) {
-      const FMCA::GradKernel laplacian_funtion(kernel_type_laplacian, sigma, 1,
-                                               i);
-
-      FMCA::Matrix Laplacian_Interior = laplacian_funtion.eval(P_sources, P);
-      Laplacian += Laplacian_Interior;
-    }
-    std::cout << "Kernel and Laplacian created" << std::endl;
-
-    FMCA::Matrix A(N, N);
-    A << Laplacian, K_bnd_n;
-
-    // FMCA::Matrix TLT = hst_sources.sampletTransform(Laplacian);
-    // TLT = hst.sampletTransform(TLT.transpose()).transpose();
-    // std::cout << "Laplacian" << std::endl;
-    // std::cout << TLT << std::endl;
-    // FMCA::Matrix TBT = hst_sources_border.sampletTransform(K_bnd_n);
-    // TBT = hst.sampletTransform(TBT.transpose()).transpose();
-    // std::cout << "Border" << std::endl;
-    // std::cout << TBT << std::endl;
-
-
-    FMCA::Vector b(N);
-    b << f, u_bc;
-
-    std::cout << "RHS created" << std::endl;
-
-    // Solver
-    Eigen::ColPivHouseholderQR<FMCA::Matrix> solver;
-    solver.compute(A);
-    // u = solution in Samplet Basis
-    FMCA::Vector alpha =
-        solver.solve(b);
-
-    // FMCA::Vector Talpha = hst.sampletTransform(alpha);
-    // std::cout << "alpha" << std::endl;
-    // std::cout << Talpha.transpose() << std::endl;
-
-    std::cout << "residual error:                          "
-              << (A * alpha - b).norm() << std::endl;
-
-    //////////////////////////////////////////////////////////////////////////////
-    const FMCA::CovarianceKernel kernel_funtion_N(kernel_type, sigma);
-    FMCA::Matrix K = kernel_funtion_N.eval(P,P);
-
-    FMCA::Vector Kalpha = K * alpha;
-
-    // std::cout << "Kalpha" << std::endl;
-    // std::cout << Kalpha.transpose() << std::endl;
-
-    std::cout << "(Kalpha - u_exact).norm() / analytical_sol.norm():    "
-              << (Kalpha - u_exact).norm() / u_exact.norm()
-              << std::endl;
-
+  FMCA::Scalar sigma = 5;
+  // FMCA::Scalar sigma = 10 * sigma_h;
+  //////////////////////////////////////////////////////////////////////////////
+  std::cout << std::string(80, '-') << std::endl;
+  std::cout << "Number of interior points = " << NPTS_SOURCE << std::endl;
+  std::cout << "Number of border points = " << NPTS_SOURCE_BORDER << std::endl;
+  std::cout << "Total number of points = " << N << std::endl;
+  std::cout << "sigma = " << sigma << std::endl;
+  //////////////////////////////////////////////////////////////////////////////
+  const FMCA::CovarianceKernel kernel_funtion_border(kernel_type, sigma);
+  FMCA::Matrix K_bnd_n = kernel_funtion_border.eval(P_sources_border, P);
+  //////////////////////////////////////////////////////////////////////////////
+  FMCA::Matrix Laplacian(NPTS_SOURCE, N);
+  Laplacian.setZero();
+  for (int i = 0; i < DIM; ++i) {
+    const FMCA::GradKernel laplacian_funtion(kernel_type_laplacian, sigma, 1,
+                                             i);
+    FMCA::Matrix Laplacian_Interior = laplacian_funtion.eval(P_sources, P);
+    Laplacian += Laplacian_Interior;
   }
+  std::cout << "Kernel and Laplacian created" << std::endl;
+
+  FMCA::Matrix A(N, N);
+  A << Laplacian, K_bnd_n;
+
+  FMCA::Vector b(N);
+  b << f, u_bc;
+
+  std::cout << "RHS created" << std::endl;
+
+  // Solver
+  Eigen::ColPivHouseholderQR<FMCA::Matrix> solver;
+  solver.compute(A);
+  // u = solution in Samplet Basis
+  FMCA::Vector alpha = solver.solve(b);
+
+  std::cout << "residual error:                          "
+            << (A * alpha - b).norm() << std::endl;
+
+  //////////////////////////////////////////////////////////////////////////////
+  const FMCA::CovarianceKernel kernel_funtion_N(kernel_type, sigma);
+  FMCA::Matrix K = kernel_funtion_N.eval(P, P);
+
+  FMCA::Vector Kalpha = K * alpha;
+
+  std::cout << "(Kalpha - u_exact).norm() / analytical_sol.norm():    "
+            << (Kalpha - u_exact).norm() / u_exact.norm() << std::endl;
+
   std::cout << std::string(80, '-') << std::endl;
   return 0;
 }
