be8ba6c261ca9e66226dc70d977eb578808bfce1
Michael Multerer <mmulterer@Mac.usilu.net>
2025-06-26 21:57:30 +0200

    seems to work

--- FMCA/src/Samplets/GraphSampletForest.h
+++ FMCA/src/Samplets/GraphSampletForest.h
@@ -46,40 +46,59 @@ class GraphSampletForest {
       points_[i] = LIsomap(sub_graphs_[i], n, emb_dim, nrgs_.data() + i);
       const SampletMoments samp_mom(points_[i], dtilde_ - 1);
       trees_[i].init(samp_mom, 0, points_[i]);
-      global_ids_[i].resize(trees_[i].block_size());
-      for (Index j = 0; j < global_ids_.size(); ++j)
-        global_ids_[i][j] = sub_graphs_[i].labels()[trees_[i].indices()[j]];
     }
     global_pos_[0] = 0;
     for (Index i = 1; i <= M_; ++i)
-      global_pos_[i] = global_pos_[i - 1] + global_ids_[i - 1].size();
+      global_pos_[i] = global_pos_[i - 1] + sub_graphs_[i - 1].labels().size();
     return;
   }
   const std::vector<Scalar> &lost_energies() { return nrgs_; }
-  Matrix sampletTransform(const Matrix &lhs) {
+
+  Matrix sampletTransform(const Matrix &lhs, const Scalar thres = 0) {
     Matrix retval(lhs.rows(), lhs.cols());
+#pragma omp parallel for schedule(dynamic)
     for (Index i = 0; i < M_; ++i) {
-      Matrix loc_rhs(global_ids_[i].size(), lhs.cols());
-      for (Index j = 0; j < global_ids_[i].size(); ++j)
-        loc_rhs.row(j) = lhs.row(global_ids_[i][j]);
+      Matrix loc_rhs = global2local(lhs, i);
+      loc_rhs = trees_[i].toClusterOrder(loc_rhs);
       loc_rhs = trees_[i].sampletTransform(loc_rhs);
-      retval.middleRows(global_pos_[i], global_ids_[i].size()) = loc_rhs;
+      if (thres > 0) {
+        Vector nrms = loc_rhs.colwise().norm();
+        for (Index j = 0; j < loc_rhs.cols(); ++j) {
+          loc_rhs.col(j) = (loc_rhs.col(j).array().abs() >= thres * nrms(j))
+                               .select(loc_rhs.col(j), 0.0);
+        }
+      }
+      retval.middleRows(global_pos_[i], sub_graphs_[i].labels().size()) =
+          loc_rhs;
     }
     return retval;
   }
 
   Matrix inverseSampletTransform(const Matrix &lhs) {
     Matrix retval(lhs.rows(), lhs.cols());
+#pragma omp parallel for schedule(dynamic)
     for (Index i = 0; i < M_; ++i) {
-      Matrix loc_rhs = trees_[i].inverseSampletTransform(
-          lhs.middleRows(global_pos_[i], global_ids_[i].size()));
-      for (Index j = 0; j < global_ids_[i].size(); ++j)
-        retval.row(global_ids_[i][j]) = loc_rhs.row(j);
+      Matrix loc_lhs =
+          lhs.middleRows(global_pos_[i], sub_graphs_[i].labels().size());
+      loc_lhs = trees_[i].inverseSampletTransform(loc_lhs);
+      loc_lhs = trees_[i].toNaturalOrder(loc_lhs);
+      local2global(retval, loc_lhs, i);
     }
     return retval;
   }
 
  private:
+  Matrix global2local(const Matrix &rhs, const Index i) {
+    Matrix loc_rhs(sub_graphs_[i].labels().size(), rhs.cols());
+    for (Index j = 0; j < sub_graphs_[i].labels().size(); ++j)
+      loc_rhs.row(j) = rhs.row(sub_graphs_[i].labels()[j]);
+    return loc_rhs;
+  }
+  void local2global(Matrix &lhs, const Matrix &loc_lhs, Index i) {
+    for (Index j = 0; j < sub_graphs_[i].labels().size(); ++j)
+      lhs.row(sub_graphs_[i].labels()[j]) = loc_lhs.row(j);
+    return;
+  }
   std::vector<Graph> sub_graphs_;
   std::vector<SampletTree> trees_;
   std::vector<Matrix> points_;
--- tests/test_GraphSampletForest.cpp
+++ tests/test_GraphSampletForest.cpp
@@ -20,19 +20,20 @@ extern "C" {
 #include <random>
 //
 #include "../FMCA/Samplets"
-#include "../FMCA/src/util/LIsomap.h"
+#include "../FMCA/src/util/IO.h"
 #include "../FMCA/src/util/Tictoc.h"
 
 using Graph = FMCA::Graph<idx_t, FMCA::Scalar>;
 
 int main(int argc, char *argv[]) {
   FMCA::Tictoc T;
-  const FMCA::Index npts = 100000;
-  const FMCA::Index k = 20;
+  // const FMCA::Index npts = 1000000;
+  const FMCA::Index k = 100;
 
   std::mt19937 mt;
   mt.seed(0);
-  FMCA::Matrix P(3, npts);
+  // FMCA::Matrix P(3, npts);
+#if 0
   {
     std::normal_distribution<FMCA::Scalar> dist(0.0, 1.0);
     for (FMCA::Index i = 0; i < P.cols(); ++i) {
@@ -40,9 +41,31 @@ int main(int argc, char *argv[]) {
       P.col(i) /= P.col(i).norm();
     }
   }
+  {
+    for (FMCA::Index i = 0; i < npts; ++i) {
+      const FMCA::Scalar u = FMCA::Scalar(rand()) / RAND_MAX;
+      const FMCA::Scalar v = FMCA::Scalar(rand()) / RAND_MAX;
+      const FMCA::Scalar t = 1.5 * FMCA_PI + 4.5 * FMCA_PI * u;
+      const FMCA::Scalar x = t * std::cos(t);
+      const FMCA::Scalar y = 21.0 * v;
+      const FMCA::Scalar z = t * std::sin(t);
+      P.col(i) << x, y, z;
+    }
+  }
+#endif
+  FMCA::Matrix data =
+      FMCA::IO::ascii2Matrix("era5_20-06-24-12h_full.txt").transpose();
+  FMCA::Matrix outline =
+      FMCA::IO::ascii2Matrix("outline.txt").transpose().topRows(3);
+  FMCA::Matrix P = data.topRows(3);
+  const FMCA::Index npts = P.cols();
   FMCA::Vector signal(P.cols());
   for (FMCA::Index i = 0; i < P.cols(); ++i)
     signal(i) = P(0, i) * P(0, i) + P(1, i) + P(2, i);
+  signal = data.row(3);
+  FMCA::IO::plotPointsColor("data.vtk", P, signal);
+  FMCA::IO::plotPoints("outline.vtk", outline);
+
   FMCA::ClusterTree CT(P, 100);
   T.tic();
   std::vector<Eigen::Triplet<FMCA::Scalar>> A = FMCA::symKNN(CT, P, k);
@@ -50,17 +73,30 @@ int main(int argc, char *argv[]) {
   FMCA::Graph<idx_t, FMCA::Scalar> G;
   G.init(P.cols(), A);
   T.tic();
-  FMCA::GraphSampletForest<Graph> gsf(G, 10, 2, 3);
+  FMCA::GraphSampletForest<Graph> gsf(G, 6, 2, 3, 1000);
   T.toc("samplet forest: ");
-  for (FMCA::Index i = 0; i < 6; ++i)
+  for (FMCA::Index i = 0; i < gsf.lost_energies().size(); ++i)
     std::cout << gsf.lost_energies()[i] << std::endl;
-  FMCA::Vector Tsignal = gsf.sampletTransform(signal);
+  T.tic();
+  FMCA::Vector Tsignal = gsf.sampletTransform(signal, 1e-3);
+  T.toc("samplet transform: ");
+  T.tic();
+  FMCA::Vector TtTsignal = gsf.inverseSampletTransform(Tsignal);
+  T.toc("inverse samplet transform: ");
+  std::cout << "isometry? " << (TtTsignal - signal).norm() / signal.norm()
+            << std::endl;
   FMCA::Index nneg = 0;
+  FMCA::Scalar nrm = Tsignal.norm();
   for (FMCA::Index i = 0; i < P.cols(); ++i) {
-    if (std::abs(Tsignal(i) * Tsignal(i)) > Tsignal.norm() * 1e-4) ++nneg;
+    if (std::abs(Tsignal(i)) > 0) ++nneg;
   }
-  Tsignal = gsf.inverseSampletTransform(Tsignal);
-  std::cout << "isometry? " << (Tsignal - signal).norm() << std::endl;
+  FMCA::IO::plotPointsColor("compression_error.vtk", P, signal - TtTsignal);
+  std::cout << (signal - TtTsignal).norm() / signal.norm() << std::endl;
+  Tsignal.setZero();
+  Tsignal(100) = 1;
+  TtTsignal = gsf.inverseSampletTransform(Tsignal);
+  FMCA::IO::plotPointsColor("samplet.vtk", P, TtTsignal);
+
   std::cout << "non negligible coeffs: " << nneg << std::endl;
 
   return 0;
