c13e86f90fb30a1f292962a5229aab8ba4087601
Michael Multerer <mmulterer@Mac.usilu.net>
2025-07-29 16:25:24 +0200

    updates tree level mapper

--- FMCA/src/Clustering/treeLevelMapper.h
+++ FMCA/src/Clustering/treeLevelMapper.h
@@ -28,15 +28,18 @@ void treeLevelMapper(const ClusterTreeBase<Derived> &ct,
   Index maxl = 0;
   Index max_id = 0;
   Index cur_level = -1;
-
+  // first determine maximum level and maximum block_id of the tree
   for (const auto &it : ct) {
     maxl = maxl > it.level() ? maxl : it.level();
     max_id = max_id > it.block_id() ? max_id : it.block_id();
   }
-
+  // s_lvl contains the offsets for each level
   s_lvl->resize(maxl + 2);
   id_map->resize(max_id + 1);
 
+  // the tree iterator does a levelwise traversal of the tree
+  // whenever a new level starts, it starting position is written to s_lvl
+  // furthermore, we have a unique mapping assigning an index to a block
   if (tree_map) {
     tree_map->resize(max_id + 1);
     for (const auto &it : ct) {
