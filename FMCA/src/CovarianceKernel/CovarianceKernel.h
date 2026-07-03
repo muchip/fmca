0eff2f8c297408a6d7515494c9fbbecade544dfc
SARA AVESANI <sara.avesani_02@studenti.univr.it>
2026-07-03 10:30:07 +0200

    TPS1D added

--- FMCA/src/CovarianceKernel/CovarianceKernel.h
+++ FMCA/src/CovarianceKernel/CovarianceKernel.h
@@ -125,6 +125,8 @@ class CovarianceKernel {
       kernel_ = [this](Scalar r) {
         return std::sqrt((r / l_) * (r / l_) + c_ * c_);
       };
+    else if (ktype_ == "TPS1D")
+      kernel_ = [this](Scalar r) { return std::pow(r / l_, 3.); };
     else if (ktype_ == "TPS2D")
       kernel_ = [this](Scalar r) { return r > 0 ? r * r * std::log(r) : 0; };
     else
