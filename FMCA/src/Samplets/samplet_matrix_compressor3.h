a3b69f8183cad9b8d09dc2643696c2be182f5615
Michael <michael@p1x13.local>
2026-07-29 14:34:03 +0200

    small tweak on stale code

--- FMCA/src/Samplets/samplet_matrix_compressor3.h
+++ FMCA/src/Samplets/samplet_matrix_compressor3.h
@@ -361,7 +361,7 @@ class SampletMatrixCompressor
             }
             const Index r_nscalfs = TR.sons(i).nscalfs();
             temp1.middleRows(r_offset, r_nscalfs).noalias() =
-                (temp2 * TC.Q()).topRows(r_nscalfs);
+                temp2.topRows(r_nscalfs) * TC.Q();
             r_offset += r_nscalfs;
             releaseMap(temp2, tid);
           }
